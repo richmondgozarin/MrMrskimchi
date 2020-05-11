676086e812d798b5937b4fba18f37e4f6b1150b8
rich gozarin <gozarra@mfcgd.com>
2020-05-12 02:05:56 +0800

    remove logs

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -5,7 +5,6 @@ function disableForm() {
     let isValid = false;
 
     inputs.forEach(element => {
-        console.log('element.value', element.value)
         if (!element.value) {
             isValid = true;
         }
@@ -17,15 +16,10 @@ function disableForm() {
 }
 
 function checkValid() {
-    console.log('fired', disableForm());
     document.orderForm.submit.disabled = disableForm();
 }
 
-console.log('inputs', inputs);
 inputs.forEach(idx => {
-    console.log('idx', idx);
-    console.log('idx', idx.value);
-
     idx.addEventListener('change', checkValid);
     idx.addEventListener('keyup', checkValid);
 })
